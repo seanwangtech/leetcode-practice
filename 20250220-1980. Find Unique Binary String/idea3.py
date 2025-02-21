160fd2c13709bb66e81c3a4cd1704376faa6ff60
Sean Wang <seanwangtech@gmail.com>
2025-02-21 10:18:26 +0000

    Add two more idea

---
+++ 20250220-1980. Find Unique Binary String/idea3.py
@@ -0,0 +1,21 @@
+from typing import *
+
+# time complexity O(n^2)
+# space complexity O(n)
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        n = len(nums)
+        nums = set(nums) # O(n)
+        def helper(nums, ans):
+            nonlocal n
+            if (len(ans) == n): 
+                if(ans not in nums): #(O(1))
+                    return ans
+                return None
+            return helper(nums, ans + '0' ) or helper(nums, ans + '1')
+        return helper(nums, '')
+        
+s = Solution()
+print(s.findDifferentBinaryString(["00","01"]))
+print(s.findDifferentBinaryString(["111","011","001"]))
+
