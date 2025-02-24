7e16cee11b954818226003b3c90a7e45240b9eb6
Sean Wang <seanwangtech@gmail.com>
2025-02-24 16:55:38 +0000

    add more ideas

--- 20250223-889. Construct Binary Tree from Preorder and Postorder Traversal/idea1.py
+++ 20250223-889. Construct Binary Tree from Preorder and Postorder Traversal/idea1.py
@@ -13,12 +13,61 @@ class TreeNode:
 
 class Solution:
     def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        pass
+        index = 0
+        indexPost = 0
+        def dfs()-> Optional[TreeNode]:
+            
+            nonlocal index, indexPost
+            if postorder[indexPost] in preorder[:index]:
+                return None 
+            if(index < len(preorder)):
+                valPre = preorder[index]
+                index += 1
+            else:
+                return None
+            
+            node = TreeNode(valPre)
+            
+            if(node.val == postorder[indexPost]):
+                indexPost += 1
+                return node
+                 
+            node.left = dfs()
+            node.right = dfs()
+            indexPost += 1
+            return node
+
+        return dfs()
+    
+    def postOrderCheck(self, postorder:List[int], root) -> bool:
+        index = 0
+        matchSoFar = True
+        def helper(node):
+            if(node is None):
+                return
+
+            if(index<len(postorder)):
+                val = postorder[index]
+                index += 1
+            else:
+                return
+            
+            if(matchSoFar):
+                postorder(node.left)
+            if(matchSoFar):
+                postorder(node.right)
+            if(val == node.val):
+                matchSoFar = True
+            else:
+                matchSoFar = False
+            
 
 s = Solution()
 
 root = s.constructFromPrePost([1,2,4,5,3,6,7],[4,5,2,6,7,3,1])
+# root = s.constructFromPrePost([3,4,1,2],[1,4,2,3])
 # print(root.left.val)
+
 def treeToList(root:TreeNode):
     q = deque()
     q.append(root)
